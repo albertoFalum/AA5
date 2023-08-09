f522a0f86136d0478cc773e8a3c8e1a5a4d388e8
albertoFalum <alberto.alum@outlook.com.ar>
2023-08-09 17:53:32 -0300

    Update AA5.java

--- src/aa5/AA5.java
+++ src/aa5/AA5.java
@@ -9,7 +9,10 @@ public class AA5 {
 
    
     public static void main(String[] args) {
-       
+        System.out.println("Hola mundo");
+     
     }
     
+    
+    ///Elimine las clases de la rama Main y el main de la Rama- Alberto
 }
