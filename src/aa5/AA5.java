99578a37c565ad261e876f5a54f36c93a54d1717
Belus8 <141675534+Belus8@users.noreply.github.com>
2023-08-09 17:38:42 -0300

    Profe
    Realizando cambios

--- src/aa5/AA5.java
+++ src/aa5/AA5.java
@@ -14,7 +14,8 @@ public class AA5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hola estoy actualizando el main");
     }
     
 }
