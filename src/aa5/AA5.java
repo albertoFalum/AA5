627ce56497bbf0f5ab7837ff9da63834faf61d5f
albertoFalum <alberto.alum@outlook.com.ar>
2023-08-09 17:26:13 -0300

    Creacion de Rama

--- src/aa5/AA5.java
+++ src/aa5/AA5.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package aa5;
 
 /**
@@ -10,11 +7,9 @@ package aa5;
  */
 public class AA5 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Holaaa");
     }
     
 }
